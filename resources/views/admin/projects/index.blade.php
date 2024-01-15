a8d69fe28e3703040736590a5289f8b8d929b558
burillu <burillu90@hotmail.it>
2024-01-15 15:06:27 +0100

    add parameter in route tu use slugs

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -18,7 +18,7 @@
                             <th scope="row">{{ $loop->iteration }}</th>
                             <td>{{ $project->title }}</td>
                             <td>{{ substr($project->body, 0, 100) . '...' }}</td>
-                            <td><button class="btn btn-primary"> <a href="{{ route('admin.projects.show', $project->id) }}">
+                            <td><button class="btn btn-primary"> <a href="{{ route('admin.projects.show', $project->slug) }}">
                                         <i class="fa-sharp fa-regular fa-eye text-white"></i></a></button><button
                                     class="btn btn-warning"> <a href="{{ route('admin.projects.edit', $project->id) }}"> <i
                                             class="fa-regular fa-pen-to-square text-white"></i></a></button><button
